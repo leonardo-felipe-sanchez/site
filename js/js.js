3a57c7929c4a7b9e7881cd7492d91078aa926bff
leonardo-felipe-sanchez <felipinhosanchez77@gmail.com>
2024-11-29 19:06:01 -0400

    fonte amesome

--- js/js.js
+++ js/js.js
@@ -1,3 +1,5 @@
+//carrosel
+
 var myIndex = 0;
 carousel();
 
@@ -12,3 +14,16 @@ function carousel() {
   x[myIndex-1].style.display = "block";  
   setTimeout(carousel, 4000); // Change image every 2 seconds
 }
+
+//navbar customizado
+
+const nav = document.getElementById("cabeca");
+window.onscroll = function() {
+  if (window.pageYOffset >= 20 || document.documentElement.scrollTop >= 20) {
+    nav.classList.add("nav-colored");
+    nav.classList.remove("nav-transparent");
+  } else {
+    nav.classList.remove("nav-colored");
+    nav.classList.add("nav-transparent");
+  }
+};
