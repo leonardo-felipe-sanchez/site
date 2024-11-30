3e318e59b34395c6049017c2bc02908d98466aa0
ifmscb <ifmscb@lab.ifms>
2024-11-29 21:53:16 -0400

    supimpa

--- js/js.js
+++ js/js.js
@@ -27,3 +27,10 @@ window.onscroll = function() {
     nav.classList.add("nav-transparent");
   }
 };
+
+//mapa
+
+function mapinha(opcao){ 
+  var imagem = document.getElementById("mapagoogle");
+  imagem.style.src= "url('https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15988604.96835149!2d-56.640899277444696!3d-11.94206478068657!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9be14f04208c27%3A0x8a25a4e379d45360!2sPAR%C3%81%20LANCHES!5e0!3m2!1spt-BR!2sbr!4v1732931131843!5m2!1spt-BR!2sbr')";
+};
