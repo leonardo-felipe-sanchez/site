41df81f828f435baae8c8f3c416ceb2073c2927b
leonardo-felipe-sanchez <felipinhosanchez77@gmail.com>
2025-03-07 14:21:14 -0400

    add pdf framework

--- js/js.js
+++ js/js.js
@@ -30,9 +30,150 @@ window.onscroll = function() {
 
 //mapa
 
-function mapinha(opcao){ 
-  var imagem = document.getElementById("mapagoogle");
-  if (opcao == 'dois') {
-    imagem.src= "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3657.7951326385855!2d-46.78176442467065!3d-23.539869778814587!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ceff633543ed97%3A0x4c0a962dfdb17dbb!2sThe%20Lucca%20Gastronomia%20%26%20Eventos%20Osasco!5e0!3m2!1spt-BR!2sbr!4v1733447239754!5m2!1spt-BR!2sbr";    
-  }
+mapboxgl.accessToken = 'pk.eyJ1IjoicG9sdmluaG8iLCJhIjoiY201aDB5ZXd4MGVwNjJrcTEwbnUwdGxiNyJ9.jtM8fnPmX8jvpwMH9CLWtA';
+    
+const map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/polvinho/cm5h5qw0c001201qf2fnfa002',
+    zoom: 8.7,
+    center: [-46.673, -23.236]
+});
+
+
+
+const geojson = {
+    'type': 'FeatureCollection',
+    'features': [
+        {
+            'type': 'Feature',
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [-47.01264331979516, -22.9799681649649]
+            },
+            'properties': {
+                'title': 'Valinhos'
+            }
+        },
+        {
+            'type': 'Feature',
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [-46.667173120458386, -23.597894151104057]
+            },
+            'properties': {
+                'title': 'Ibirapuera'
+            }
+        },
+        {
+            'type': 'Feature',
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [-46.779183278445885, -23.539832844553104]
+            },
+            'properties': {
+                'title': 'Osasco'
+            }
+        },
+        {
+            'type': 'Feature',
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [-46.64794921875, -23.574686305893124]
+            },
+            'properties': {
+                'title': 'Paraiso'
+            }
+        },
+        {
+            'type': 'Feature',
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [-46.76956931414378, -23.610161856231272]
+            },
+            'properties': {
+                'title': 'Taboão da Serra'
+            }
+        },
+        {
+            'type': 'Feature',
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [-46.65773118491478, -23.568788021779525]
+            },
+            'properties': {
+                'title': 'Jardins'
+            }
+        },
+        {
+            'type': 'Feature',
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [-46.99873934064641, -22.699237679386684]
+            },
+            'properties': {
+                'title': 'Jaguariuna'
+            }
+        }
+        // Add your other 6 points here
+    ]
+};
+
+// Add markers to the map
+for (const feature of geojson.features) {
+    // Create a DOM element for each marker
+    const el = document.createElement('div');
+    el.className = 'marker';
+
+    // Add markers to the map
+    new mapboxgl.Marker(el)
+        .setLngLat(feature.geometry.coordinates)
+        .addTo(map);
+}
+
+function Paraiso() {
+  map.flyTo({
+center: [-46.64794921875, -23.574686305893124],
+zoom: 20
+});
+}
+function Jaguariuna() {
+  map.flyTo({
+center: [-46.99873934064641, -22.699237679386684],
+zoom: 20
+});
+}
+
+function Jardins() {
+  map.flyTo({
+center: [-46.65773118491478, -23.568788021779525],
+zoom: 20
+});
+}
+
+function Taboao() {
+  map.flyTo({
+center: [-46.76956931414378, -23.610161856231272],
+zoom: 20
+});
+}
+
+function Osasco() {
+  map.flyTo({
+center: [-46.779183278445885, -23.539832844553104],
+zoom: 20
+});
+}
+
+function Ibirapuera() {
+  map.flyTo({
+center: [-46.667173120458386, -23.597894151104057],
+zoom: 20
+});
+}
+          
+function Valinhos() {
+  map.flyTo({
+center: [-47.01264331979516, -22.9799681649649],
+zoom: 20
+});
 }
